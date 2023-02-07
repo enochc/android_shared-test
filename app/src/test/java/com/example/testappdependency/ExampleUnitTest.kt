bbf93058ca3f4782461677cc53fd728184ae260f
Enoch Carter <Enoch.Carter@snapav.com>
2023-02-07 14:01:01 -0700

    no message

--- app/src/test/java/com/example/testappdependency/ExampleUnitTest.kt
+++ app/src/test/java/com/example/testappdependency/ExampleUnitTest.kt
@@ -1,33 +1,19 @@
 package com.example.testappdependency
 
 
-import android.util.Log
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.sharedtestcode.SharedTestStringClass
 
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(RobolectricTestRunner::class)
-@Config(
-    instrumentedPackages = [
-        // required to access final members on androidx.loader.content.ModernAsyncTask
-        "androidx.loader.content"
-    ]
-)
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
     @Test
     fun ensureCorrectValueFromSharedTestCode() {
